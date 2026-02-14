392aec8c300b19a160586583aaf3dbe58998a005
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2026-02-14 23:48:55 +0500

    style: animation to render video

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
       animation: {
         "pulse-ring": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        "fade-in-render": "fadeIn 1s ease-out forwards",
       },
     },
   },
